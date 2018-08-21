d7707517453d2c2f3fbfdb2d54ba773bf6f8bf07
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-08-21 08:38:16 -0500

    fixed hint images, messed with spacing

--- logic.js
+++ logic.js
@@ -2,8 +2,9 @@ function evaporate() {
     document.getElementById("start").innerHTML = "";
     document.getElementById("start2").innerHTML = "";
     document.getElementById("button1").innerText = "Good Luck";
+    document.getElementById("button2").style.visibility = "visible";
 }
-// onclick=evaporate()
+
 var monstersArray = [
     "Doppelganger",
     "Dire Wolf",
@@ -24,7 +25,8 @@ var monstersArray = [
     "Skeleton",
     "Werewolf",
     "Weretiger",
-    "Owlbear"];
+    "Owlbear"
+];
 
 var userText = document.getElementById("user-text");
 var letter1 = document.getElementById("underline1");
@@ -51,7 +53,73 @@ function chooseWord() {
     document.getElementById("underline13").innerHTML = rand[12];
     document.getElementById("underline14").innerHTML = rand[13];
     document.getElementById("underline15").innerHTML = rand[14];
-//i'm not sure what goes here. some kind of loop?    
+    //i'm not sure what goes here. some kind of loop?    
 
-    
+
+};
+function showHint() {
+    document.getElementById("wordImage").style.visibility = "visible";
+
+}
+function matchImages() {
+    if (rand === "Werewolf") {
+        document.getElementById("wordImage").src = "assets/images/Werewolf-5e - Copy.png";
+    }
+    if (rand === "Owlbear") {
+        document.getElementById("wordImage").src = "assets/images/Owlbear_ - Copy.jpg";
+    }
+    if (rand === "Weretiger") {
+        document.getElementById("wordImage").src = "assets/images/Weretiger-5e - Copy.png";
+    }
+    if (rand === "Skeleton") {
+        document.getElementById("wordImage").src = "assets/images/Skeleton-5e - Copy.jpg";
+    }
+    if (rand === "Pseudodragon") {
+        document.getElementById("wordImage").src = "assets/images/Pseudodragon-5e - Copy.png";
+    }
+    if (rand === "Necromancer") {
+        document.getElementById("wordImage").src = "assets/images/necromancer - Copy.jpg";
+    }
+    if (rand === "Manticore") {
+        document.getElementById("wordImage").src = "assets/images/Manticore - Copy.jpg";
+    }
+    if (rand === "Elder Lich") {
+        document.getElementById("wordImage").src = "assets/images/lich - Copy.jpg";
+    }
+    if (rand === "Kobold") {
+        document.getElementById("wordImage").src = "assets/images/Kobold_ - Copy.jpg";
+    }
+    if (rand === "Iron Golem") {
+        document.getElementById("wordImage").src = "assets/images/Iron_golem-5e - Copy.jpg";
+    }
+    if (rand === "Hell Hound") {
+        document.getElementById("wordImage").src = "assets/images/Hell-hound-5e - Copy.png";
+    }
+    if (rand === "Griffon") {
+        document.getElementById("wordImage").src = "assets/images/Griffon-5e - Copy.png";
+    }
+    if (rand === "Mind Flayer") {
+        document.getElementById("wordImage").src = "assets/images/Mind_flayer - Copy.jpg";
+    }
+    if (rand === "Gnoll") {
+        document.getElementById("wordImage").src = "assets/images/Gnoll-5e - Copy.png";
+    }
+    if (rand === "Giant Spider") {
+        document.getElementById("wordImage").src = "assets/images/Giant_spider-5e - Copy.jpg";
+    }
+    if (rand === "Gelatinous Cube") {
+        document.getElementById("wordImage").src = "assets/images/Gelatinous_Cube - Copy.jpg";
+    }
+    if (rand === "Air Elemental") {
+        document.getElementById("wordImage").src = "assets/images/Air_elemental_5e - SIL.jpg";
+    }
+    if (rand === "Goblin") {
+        document.getElementById("wordImage").src = "assets/images/goblin - Copy.jpg";
+    }
+    if (rand === "Dire Wolf") {
+        document.getElementById("wordImage").src = "assets/images/dire-wolf - SIL.jpg";
+    }
+    if (rand === "Doppelganger") {
+        document.getElementById("wordImage").src = "assets/images/Doppelganger-5e - Copy.jpg";
+    }
 };
