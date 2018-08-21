c96d837a4e758d63b66fd368d9faf5a4c35181c2
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-08-21 03:51:41 -0500

    beginning codework, start button functioning + guess letter function

---
+++ logic.js
@@ -0,0 +1,6 @@
+function evaporate() {
+    document.getElementById("start").innerHTML = "";
+    document.getElementById("start2").innerHTML = "";
+    document.getElementById("button1").innerText = "Good Luck";
+}
+// onclick=evaporate()
